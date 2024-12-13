f62e9c0f535ae8217e2f6ff231762a8335acd7a1
Yurochkina <olga.yurochkina@gmail.com>
2024-12-13 12:34:22 -0500

    Added react app with ts.

---
+++ frontend/src/views/AllProducts.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const AllProducts: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>Product List</h1>
+    </div>
+  );
+};
+
+export default AllProducts;
