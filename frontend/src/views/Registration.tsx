dc182281947d39299c9dbe334370ca167aa3057d
Yurochkina <olga.yurochkina@gmail.com>
2024-12-13 12:34:22 -0500

    Added react app with ts.

---
+++ frontend/src/views/Registration.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const Registration: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default Registration;
