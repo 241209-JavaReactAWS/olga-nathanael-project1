413f5f5fb03f92c294fc1277b7a37c2c06d42c88
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-12-15 12:21:02 -0500

    fixed base URL

--- frontend/src/postman.ts
+++ frontend/src/postman.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const postman = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://localhost:8080/api/v1',
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json'
