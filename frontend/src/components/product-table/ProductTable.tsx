a1cc7c0749c16d74e120e373258b50d17d1f3bec
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-12-16 16:18:47 -0500

    refactoring into components and adding error message if user is not admin

---
+++ frontend/src/components/product-table/ProductTable.tsx
@@ -0,0 +1,73 @@
+import {useCallback, useEffect, useState} from 'react'
+import IProduct from '../IProduct'
+import {postman} from '../../postman'
+import ProductTableRow from '../product-table-row/ProductTableRow'
+import NewProductTableRow from '../new-product-table-row/NewProductTableRow'
+import Snackbar, {SnackbarStyle} from '../snackbar/Snackbar'
+import "./ProductTable.css"
+
+export default function ProductTable() {
+    const [products, setProducts] = useState<Array<IProduct>>([])
+    const [error, setError] = useState<string>('')
+    const [success, setSuccess] = useState<string>('')
+
+    const onError = (message: string) => {
+        setError(message)
+        setTimeout(() => setError(''), 5000)
+    }
+
+    const onSuccess = (message: string) => {
+        setSuccess(message)
+        updateProductsListing()
+        setTimeout(() => setSuccess(''), 5000)
+    }
+
+    const updateProductsListing = useCallback(() => {
+        postman.get('/products')
+            .then((response) => {
+                setProducts(response.data)
+            }).catch((error) => {
+            console.log(error)
+            onError('Something\'s not working right now. Please try again later.')
+        })
+    }, [setProducts])
+
+    useEffect(() => {
+        updateProductsListing()
+    }, [updateProductsListing])
+
+    return <>
+        <h2>Products</h2>
+        <table id="productTable">
+            <thead>
+            <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Quantity On Hand</th>
+                <th>Options</th>
+            </tr>
+            </thead>
+            <tbody>
+            {products.map((product) => <ProductTableRow product={product} key={product.id} onSuccess={onSuccess}
+                                                        onError={onError}/>)}
+            <tr>
+                <td>
+                    <div>
+                        <h3 style={{paddingLeft: '10px'}}>Add new product...</h3>
+                    </div>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <NewProductTableRow onSuccess={onSuccess} onError={onError}/>
+            </tbody>
+        </table>
+        <Snackbar open={Boolean(error)} style={SnackbarStyle.ERROR} message={error}/>
+        <Snackbar open={Boolean(success)} style={SnackbarStyle.SUCCESS} message={success}/>
+    </>
+}
--- frontend/src/views/admin/Admin.tsx
+++ frontend/src/views/admin/Admin.tsx
@@ -1,80 +1,20 @@
-import React, {useCallback, useEffect, useState} from 'react'
-import IProduct from '../../components/IProduct'
 import './Admin.css'
-import ProductTableRow from '../../components/product-table-row/ProductTableRow'
-import NewProductTableRow from '../../components/new-product-table-row/NewProductTableRow'
-import {postman} from '../../postman'
-import Snackbar, {SnackbarStyle} from '../../components/snackbar/Snackbar'
+import {useAuth} from '../../hooks/useAuth'
+import ProductTable from '../../components/product-table/ProductTable'
 
-interface Props {
-}
+export default function Admin() {
+    const auth = useAuth()
 
-const Admin: React.FC<Props> = () => {
-    const [products, setProducts] = useState<Array<IProduct>>([])
-    const [error, setError] = useState<string>('')
-    const [success, setSuccess] = useState<string>('')
-
-    const onError = (message: string) => {
-        setError(message)
-        setTimeout(() => setError(''), 5000)
-    }
-
-    const onSuccess = (message: string) => {
-        setSuccess(message)
-        updateProductsListing()
-        setTimeout(() => setSuccess(''), 5000)
-    }
-
-    const updateProductsListing = useCallback(() => {
-        postman.get('/products')
-            .then((response) => {
-                setProducts(response.data)
-            }).catch((error) => {
-            console.log(error)
-            onError('Something\'s not working right now. Please try again later.')
-        })
-    }, [setProducts])
-
-    useEffect(() => {
-        updateProductsListing()
-    }, [updateProductsListing])
+    console.log(auth.role)
 
     return <div id="adminView">
-        <h1>Admin</h1>
-        <h2>Products</h2>
-        <table id="productTable">
-            <thead>
-            <tr>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Quantity On Hand</th>
-                <th>Price</th>
-                <th>Options</th>
-            </tr>
-            </thead>
-            <tbody>
-            {products.map((product) => <ProductTableRow product={product} key={product.id} onSuccess={onSuccess}
-                                                        onError={onError}/>)}
-            <tr>
-                <td>
-                    <div>
-                        <h3 style={{paddingLeft: '10px'}}>Add new product...</h3>
-                    </div>
-                </td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <NewProductTableRow onSuccess={onSuccess} onError={onError}/>
-            </tbody>
-        </table>
-        <Snackbar open={Boolean(error)} style={SnackbarStyle.ERROR} message={error}/>
-        <Snackbar open={Boolean(success)} style={SnackbarStyle.SUCCESS} message={success}/>
+        <h1 id='adminHeader'>Admin Portal</h1>
+        {auth.role === 'admin'
+            ? <ProductTable />
+            : <div id='unauthorizedDiv'>
+                <h1>You are not authorized to view this page.</h1>
+            </div>
+        }
     </div>
 
 }
-
-export default Admin
